8e8eeecf3dc2df8002d5c278866bff418e2af04d
Felippe Marques <ambrsp@hotmail.com>
2022-04-17 22:55:11 -0300

    Módulo : validações 1/3

---
+++ Atividades/A5/Modulo/src/js/main.js
@@ -0,0 +1,49 @@
+
+const masks = {
+    nome(value){
+        console.log(value)
+        return value.replace(/\d/g, "");
+    },
+    cpf(value){
+        console.log(value)
+
+        return value.replace(/\D/g,"")
+        .replace(/(\d{3})(\d)/,"$1.$2")
+        .replace(/(\d{3})(\d)/,"$1.$2")
+        .replace(/(\d{3})(\d)/,"$1-$2")
+        .replace(/(-\d{2})\d/,"$1")
+    },
+    date(value){
+        console.log(value)
+        return value.replace(/\D/g,"")
+        .replace(/(\d{2})(\d)/,"$1/$2")
+        .replace(/(\d{2})(\d)/,"$1/$2")
+        .replace(/(\/\d{4})(\d)/,"$1$2")
+        .replace(/(\d{4})\d+?$/,"$1")
+    },
+    fone(value){
+        console.log(value)
+        return value.replace(/\D/g,"")
+        .replace(/(\d{2})(\d)/, "($1) $2")
+        .replace(/(\d{5})(\d)/, "$1-$2")
+        .replace(/(\d{4})/, "$1")
+        .replace(/(-\d{4})\d+?$/, "$1")
+    },
+    cep(value){
+        console.log(value)
+        return value.replace(/\D/g,"")
+        .replace(/(\d{5})(\d)/,"$1-$2")
+        .replace(/(\d{3})(\d)/,"$1$2")
+        .replace(/(-\d{3})\d+?$/,"$1")
+    }
+}
+
+
+document.querySelectorAll("input").forEach(($input =>{
+    const field = $input.dataset.js
+
+    $input.addEventListener("input", (e) =>{
+        
+        e.target.value = masks[field](e.target.value);
+    },false)
+}))
